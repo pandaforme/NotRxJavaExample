10d3ef0644c247750542e2adede4a82bb9b825e5
ChienKu Chen <chienku_chen@htc.com>
2015-08-10 16:25:28 +0800

    Async version

--- src/main/java/Api.java
+++ src/main/java/Api.java
@@ -2,7 +2,19 @@ import java.net.URI;
 import java.util.List;
 
 public interface Api {
-    List<Cat> queryCats(String query);
+    List<Cat> queryCats(String query, CatsQueryCallback catsQueryCallback);
 
-    URI store(Cat cat);
+    URI store(Cat cat, StoreCallback storeCallback);
+
+    interface CatsQueryCallback {
+        void onCatListReceived(List<Cat> cats);
+
+        void onQueryFailed(Exception e);
+    }
+
+    interface StoreCallback {
+        void onCatStored(URI uri);
+
+        void onStoreFailed(Exception e);
+    }
 }
--- src/main/java/CatsHelper.java
+++ src/main/java/CatsHelper.java
@@ -3,17 +3,40 @@ import java.util.Collections;
 import java.util.List;
 
 public class CatsHelper {
-
     Api api;
 
-    public URI saveTheCutestCat(String query) {
-        List<Cat> cats = api.queryCats(query);
-        Cat cutest = findCutest(cats);
-        URI savedUri = api.store(cutest);
-        return savedUri;
+    public void saveTheCutestCat(String query, final CutestCatCallback cutestCatCallback) {
+        api.queryCats(query, new Api.CatsQueryCallback() {
+            @Override
+            public void onCatListReceived(List<Cat> cats) {
+                Cat cutest = findCutest(cats);
+                api.store(cutest, new Api.StoreCallback() {
+                    @Override
+                    public void onCatStored(URI uri) {
+                        cutestCatCallback.onCutestCatSaved(uri);
+                    }
+
+                    @Override
+                    public void onStoreFailed(Exception e) {
+                        cutestCatCallback.onError(e);
+                    }
+                });
+            }
+
+            @Override
+            public void onQueryFailed(Exception e) {
+                cutestCatCallback.onError(e);
+            }
+        });
     }
 
     private Cat findCutest(List<Cat> cats) {
         return Collections.max(cats);
+    }
+
+    public interface CutestCatCallback {
+        void onCutestCatSaved(URI uri);
+
+        void onError(Exception e);
     }
 }
